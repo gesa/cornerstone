a81292ce92abc1ce96cc882e19f97acc4490a1e4
gesa <abraun@barackobama.com>
2013-03-13 14:23:49 -0500

    boilerplate first commit

---
+++ js/_script.js
@@ -0,0 +1,12 @@
+(function ($, doc, win) {
+    "use strict";
+//    var;
+
+    function init() {
+    }
+
+    $(doc).ready(function () {
+        init();
+    });
+
+})(jQuery, document, window);
